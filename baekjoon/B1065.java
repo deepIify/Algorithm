b88dc243dcfeb8b418792835f5fd7940d8997ce9
shushistack <kjm71712@naver.com>
2021-05-28 21:56:59 +0900

    한수, string으로 잘라서 판단하는 간단한 문제

---
+++ baekjoon/B1065.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+
+public class B1065 {
+    static int N;
+    static int[] han = new int[1001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        initHan();bw.write(han[N] + "\n");
+        bw.flush();bw.close();
+
+    }
+    public static void initHan() {
+        for (int i = 1; i < 100; i++) {
+            han[i] = i;
+        }
+        String number, numbers[];
+        for (int i = 100; i < 1000; i++) {
+            number = "" + i;
+            numbers = number.split("");
+            int term = Integer.parseInt(numbers[1]) - Integer.parseInt(numbers[0]);
+            han[i] = ((Integer.parseInt(numbers[0]) + 2 * term) == Integer.parseInt(numbers[2])) ?
+                    han[i - 1] + 1 : han[i - 1];
+        }
+        han[1000] = han[999];
+    }
+}
