e135310d742ac3f18254cb45b4f0749856d3e537
shushistack <kjm71712@naver.com>
2021-06-04 19:45:13 +0900

    Ax + Bsin(x) = C

---
+++ baekjoon/B13705.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B13705 {
+    static int A, B, C , n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();B = sc.nextInt(); C = sc.nextInt();
+        double t = A * Math.PI * n;
+        double t2 = B * n;
+    }
+}
