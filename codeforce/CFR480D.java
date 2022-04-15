7b4428a75e97bbbe612da55b4e7432adfc292cbe
shushistack <kjm71712@naver.com>
2022-04-15 21:10:17 +0900

    Add Algorithm Problem

---
+++ codeforce/CFR480D.java
@@ -0,0 +1,13 @@
+package codeforce;
+
+import java.io.*;
+
+public class CFR480D {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();bw.close();
+    }
+}
