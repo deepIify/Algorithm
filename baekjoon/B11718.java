f1263a7ee433ed87c11ede8dca707a69925f7fd4
shushistack <kjm71712@naver.com>
2022-06-16 21:25:39 +0900

    그대로 출력하기, 간단한 입력 문제

---
+++ baekjoon/B11718.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B11718 {
+    static String[] str = new String[101];
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        Arrays.fill(str, "");
+    }
+}
