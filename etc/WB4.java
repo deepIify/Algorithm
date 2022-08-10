ae9cd6f9bb4ce087a44eed52e3b7b71b053143b6
shushistack <kjm71712@naver.com>
2022-08-10 19:23:41 +0900

    Add Problem

---
+++ etc/WB4.java
@@ -0,0 +1,17 @@
+package etc;
+
+import java.io.*;
+
+public class WB4 {
+    static int n, v = 0, ans = 0;
+    static String ins[], in;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ins = br.readLine().split(" ");
+
+
+        bw.write(ans + "\n");
+        bw.flush();bw.close();
+    }
+}
